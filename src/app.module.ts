255f29a70bee3765c39e8763a2cae57c1d062c78
zlocate <43764701+zlocate@users.noreply.github.com>
2020-11-23 00:37:26 +0300

    Fix deprecation warning

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { StudentsModule } from './students/students.module';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/student-list'),
+    MongooseModule.forRoot('mongodb://localhost:27017/student-list', {
+      useFindAndModify: false,
+    }),
     StudentsModule,
   ],
   controllers: [],
